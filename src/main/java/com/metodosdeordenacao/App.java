6f5fb2fd622d63e17634f52ad88d8d8b0834e821
Miguel <mgehring@gvdasa.com.br>
2026-05-17 23:09:41 -0300

    feat: configuração ambiente

---
+++ src/main/java/com/metodosdeordenacao/App.java
@@ -0,0 +1,9 @@
+package com.metodosdeordenacao;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("=== Métodos de Ordenação ===");
+        System.out.println("Aplicação iniciada com sucesso!fafasfassfas");
+    }
+}
